18e5a212e1f4ec6f0a9ef049b8116d9684dc981e
KazimDandir <kikbal.dandir@gmail.com>
2024-04-16 14:17:25 +0300

    Asp.net Edu

---
+++ HelloWorld/HelloWorld/Controllers/UrunController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        //Urun/Liste
+        public ViewResult Liste()
+        {   
+            return View(); 
+        }
+
+        public ViewResult UrunDetay()
+        {
+            return View();
+        }
+    }
+}
