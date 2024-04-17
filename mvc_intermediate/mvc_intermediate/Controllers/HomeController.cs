104dc1b43b45acefa3134a8acdcd50ad7027e2d9
KazimDandir <kikbal.dandir@gmail.com>
2024-04-17 13:04:03 +0300

    razor

---
+++ mvc_intermediate/mvc_intermediate/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_intermediate.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //veritabanına bağlanır ve bilgileri getirir
+            //getirilen bilgiler bir model içerisine yani bir sınıf içerisine aktarılır ve model view'e gönderilir
+            return View();
+        }
+    }
+}
