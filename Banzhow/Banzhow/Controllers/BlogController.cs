7da331a5745c891157131acb8dbf398916a66351
KazimDandir <kikbal.dandir@gmail.com>
2024-04-17 09:45:15 +0300

    continue edu

---
+++ Banzhow/Banzhow/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Banzhow.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            //veritabanından bütün blog bilgilerini al ve ve view üzerine gönder
+            //kendisine gelen blog bilgilerini dinamik içerik üreten view yapısı bunu statik html'e çevirir
+            //bloglar
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Banzhow/Banzhow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Banzhow.MvcApplication" Language="C#" %>
