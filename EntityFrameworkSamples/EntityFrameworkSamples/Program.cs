bd730779fe9cdbd42ab7de449b6d0b2c12b5a03e
KazimDandir <kikbal.dandir@gmail.com>
2024-05-20 22:35:35 +0300

    updating-data

--- EntityFrameworkSamples/EntityFrameworkSamples/Program.cs
+++ EntityFrameworkSamples/EntityFrameworkSamples/Program.cs
@@ -12,26 +12,72 @@ namespace EntityFrameworkSamples
     {
         static void Main(string[] args)
         {
-            // LINQ (Language integrated query)
+            #region Inserting Data
+            //// LINQ (Language integrated query)
+            //UrunContext context = new UrunContext();
+
+            //List<Urun> urunler = new List<Urun>()
+            //{
+            //    new Urun(){UrunAdi="Samsung S4", Fiyat = 2000, StokAdeti = 100, Satistami = true},
+            //    new Urun(){UrunAdi="Samsung S5", Fiyat = 3000, StokAdeti = 200, Satistami = false},
+            //    new Urun(){UrunAdi="Samsung S6", Fiyat = 4000, StokAdeti = 300, Satistami = true},
+            //    new Urun(){UrunAdi="Samsung S7", Fiyat = 5000, StokAdeti = 400, Satistami = false},
+            //    new Urun(){UrunAdi="Samsung S8", Fiyat = 6000, StokAdeti = 100, Satistami = true}
+            //};
+
+            //foreach (var urun in urunler)
+            //{
+            //    context.Urunler.Add(urun);
+            //}
+
+            //context.SaveChanges();
+
+            //Console.WriteLine("kayıt edildi");
+            #endregion
+
+            #region Updating Data
+
             UrunContext context = new UrunContext();
 
-            List<Urun> urunler = new List<Urun>()
-            {
-                new Urun(){UrunAdi="Samsung S4", Fiyat = 2000, StokAdeti = 100, Satistami = true},
-                new Urun(){UrunAdi="Samsung S5", Fiyat = 3000, StokAdeti = 200, Satistami = false},
-                new Urun(){UrunAdi="Samsung S6", Fiyat = 4000, StokAdeti = 300, Satistami = true},
-                new Urun(){UrunAdi="Samsung S7", Fiyat = 5000, StokAdeti = 400, Satistami = false},
-                new Urun(){UrunAdi="Samsung S8", Fiyat = 6000, StokAdeti = 100, Satistami = true}
-            };
+            //var urun = context.Urunler.Find(1);
+
+            //Console.WriteLine("Urun Id : {0} Urun Adı : {1} Fiyat : {2}", urun.Id, urun.UrunAdi, urun.Fiyat);
+
+            //urun.Fiyat = urun.Fiyat + (urun.Fiyat * 0.5);
+            //urun.UrunAdi = "Samsung S6";
+            //urun.StokAdeti += 100;
+
+            //context.SaveChanges();
+
+            //urun = context.Urunler.Find(1);
+
+            //Console.WriteLine("Urun Id : {0} Urun Adı : {1} Fiyat : {2}", urun.Id, urun.UrunAdi, urun.Fiyat);
+
+            var urunler = context.Urunler.ToList();
 
             foreach (var urun in urunler)
             {
-                context.Urunler.Add(urun);
+                Console.WriteLine("Fiyat : {0}", urun.Fiyat);
+            }
+
+            Console.WriteLine("--------------");
+
+            foreach (var urun in urunler)
+            {
+                urun.Fiyat *= 1.5;
             }
 
             context.SaveChanges();
 
-            Console.WriteLine("kayıt edildi");
+            urunler = context.Urunler.ToList();
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine("Fiyat : {0}", urun.Fiyat);
+            }
+
+            #endregion
+
             Console.ReadLine();
         }
     }
