ee379d5734eb1c71a2a4e8c04805a2c90b9cdaa5
KazimDandir <kikbal.dandir@gmail.com>
2024-05-20 11:22:32 +0300

    entity-class

--- EntityFrameworkSamples/EntityFrameworkSamples/Program.cs
+++ EntityFrameworkSamples/EntityFrameworkSamples/Program.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//İlk olarak NuGet üzerinden EntityFramework install edildi.
+
 namespace EntityFrameworkSamples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //NuGet üzerinden EntityFramework install edildi.
+            Kategori k = new Kategori();
+            k.Id = 4;
+            k.KategoriAdi = "beyaz eşya";
 
+            List<Kategori> kategoriler = new List<Kategori>()
+            {
+                new Kategori(){Id=1, KategoriAdi="telefon"},
+                new Kategori(){Id=1, KategoriAdi="bilgisayar"},
+                new Kategori(){Id=1, KategoriAdi="tablet"}
+            };
+
+            kategoriler.Add(k);
         }
     }
 }
