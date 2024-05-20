c75008d76649d55bacc0ac48f9a12431b623b022
KazimDandir <kikbal.dandir@gmail.com>
2024-05-20 16:40:30 +0300

    inserting-data

--- EntityFrameworkSamples/EntityFrameworkSamples/Program.cs
+++ EntityFrameworkSamples/EntityFrameworkSamples/Program.cs
@@ -12,14 +12,26 @@ namespace EntityFrameworkSamples
     {
         static void Main(string[] args)
         {
-            Kategori k = new Kategori();
-            k.KategoriAdi = "telefon";
+            // LINQ (Language integrated query)
+            UrunContext context = new UrunContext();
 
-            UrunContext db = new UrunContext();
-            db.Kategoriler.Add(k);
-            db.SaveChanges();
+            List<Urun> urunler = new List<Urun>()
+            {
+                new Urun(){UrunAdi="Samsung S4", Fiyat = 2000, StokAdeti = 100, Satistami = true},
+                new Urun(){UrunAdi="Samsung S5", Fiyat = 3000, StokAdeti = 200, Satistami = false},
+                new Urun(){UrunAdi="Samsung S6", Fiyat = 4000, StokAdeti = 300, Satistami = true},
+                new Urun(){UrunAdi="Samsung S7", Fiyat = 5000, StokAdeti = 400, Satistami = false},
+                new Urun(){UrunAdi="Samsung S8", Fiyat = 6000, StokAdeti = 100, Satistami = true}
+            };
 
-            Console.WriteLine("veri kayıt edildi");
+            foreach (var urun in urunler)
+            {
+                context.Urunler.Add(urun);
+            }
+
+            context.SaveChanges();
+
+            Console.WriteLine("kayıt edildi");
             Console.ReadLine();
         }
     }
